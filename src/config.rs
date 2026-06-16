cc210f0065a8642d42ac31d477b09ca84914e090
prob1d <mori@probd.cn>
2026-06-16 09:12:05 +0800

    Update

--- src/config.rs
+++ src/config.rs
@@ -81,7 +81,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), "Qm12345.".to_string());
+        map.insert("password".to_string(), "q".to_string());
         RwLock::new(map)
     };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
