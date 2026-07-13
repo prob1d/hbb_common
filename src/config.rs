8ec30afd719fb9f416f573c062cf49b604dc7f43
prob1d <mori@probd.cn>
2026-07-13 10:57:04 +0800

    Update RENDEZVOUS_SERVERS to new domain

--- src/config.rs
+++ src/config.rs
@@ -123,7 +123,7 @@ const CHARS: &[char] = &[
 
 // pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 // pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.chelmey.cn"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.chelmey.net"];
 pub const RS_PUB_KEY: &str = "";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
